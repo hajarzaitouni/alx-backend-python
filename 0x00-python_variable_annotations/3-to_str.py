e175c2ebcd7d16e7d86c7f53a0562a8aceaed5cf
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-06-06 21:37:23 +0100

    convert af loat to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Converts a float to its string representation
+
+Args:
+n (float): the float number to convert.
+"""
+
+
+def to_str(n: float) -> str:
+    """ returns the string representation of the float """
+    return str(n)
