95392893f4ac145bba23028efd1c7f33ebefc4d0
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-06-06 23:36:30 +0100

    return sum of list of float numbers

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+from typing import List
+"""
+returns a sum of list of float
+
+Args:
+    input_list (List[float]): A list of float numbers.
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ returns the sum of a list of floats. """
+    return sum(input_list)
