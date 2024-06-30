7c7e5a7aaf268d0d54833efb9b68997fa0c69a28
Hajar Zaitouni <hajarzaitouni88@gmail.com>
2024-06-30 17:36:27 +0100

    update test_utils file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -42,6 +42,34 @@ class TestGetJson(unittest.TestCase):
         mock_resp.json.return_value = test_payload
         mock_get.return_value = mock_resp
         self.assertEqual(get_json(test_url), test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """ Unit test class for the memoize decorator. """
+    def test_memoize(self,):
+        """ Test the momoize decorator """
+        class TestClass:
+            """ nested class """
+            def a_method(self):
+                """ return a value of 42 """
+                return 42
+
+            @memoize
+            def a_property(self):
+                """ a property """
+                return self.a_method()
+        with patch.object(TestClass, 'a_method', return_value=42) as mocked:
+            test_class = TestClass()
+
+            # Call the memoized property twice
+            r1 = test_class.a_property
+            r2 = test_class.a_property
+
+            # Assert that a_method was called only once
+            mocked.assert_called_once()
+
+            self.assertEqual(r1, 42)
+            self.assertEqual(r2, 42)
 
 
 if __name__ == '__main__':
